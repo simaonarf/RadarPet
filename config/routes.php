3c304e1a93317ba3539aed5c2e09dd48fa3163bc
simaonarf <simaofranciel1@gmail.com>
2025-10-03 21:14:38 -0300

    chore: up code config

---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Controllers\HomeController;
+use App\Controllers\AuthController;
+use Core\Router\Route;
+
+// Home
+Route::get('/', [HomeController::class, 'index'])->name('root');
+
+// Authentication
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::get('/login', [AuthController::class, 'showlogin'])->name('login');
