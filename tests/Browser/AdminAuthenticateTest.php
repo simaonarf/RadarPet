a3346e6eff5eed41932e164dd0bc59ad6e10d50f
alanplima29 <alanlima@alunos.utfpr.edu.br>
2025-10-12 19:53:09 -0300

    testes atulizados2

--- tests/Browser/AdminAuthenticateTest.php
+++ tests/Browser/AdminAuthenticateTest.php
@@ -4,14 +4,17 @@ namespace Tests\Browser;
 
 use Tests\TestCase;
 
-class IndexTest extends TestCase
+class AdminAuthenticateTest extends TestCase
 {
     public function test_should_redirect_if_not_authenticated(): void
     {
         $page = file_get_contents('http://web/admin/dashboard');
         $this->assertTrue(http_response_code(302));
 
+        $statuscode = $http_response_header[0];
         $location = $http_response_header[10];
+
+        $this->assertEquals('HTTP/1.1 302 Found', $statuscode);
         $this->assertEquals('Location: /login', $location);
     }
 }
