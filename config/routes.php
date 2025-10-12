fa3213d2b08d36bafcf4385330513f7a3d870131
alanplima29 <alanlima@alunos.utfpr.edu.br>
2025-10-12 16:15:03 -0300

    Testando Middleware

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
-use Core\Router\Route; 
+
+use Core\Router\Route;
 
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
@@ -9,12 +10,16 @@ use App\Controllers\AdminController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-// ROTAS DE AUTENTICAÇÃO
-
-// Route::get('/register', [AuthController::class, 'showRegister'])->name('register'); 
+// ROTAS DE AUTENTICACAO
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
-Route::get('/logout', [AuthController::class, 'destroy'])->name('logout');
 
-// ROTAS DO PAINEL ADM
-Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    // Route::get('/register', [AuthController::class, 'showRegister'])->name('register'); 
+    Route::get('/logout', [AuthController::class, 'destroy'])->name('logout');
+
+    // ROTAS DO PAINEL ADM
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
